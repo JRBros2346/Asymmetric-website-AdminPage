3cf5eac39748876c34f522a9f4fb878c36e7fe47
cosmic-striker <vishald.cs2023@citchennai.net>
2024-11-19 08:45:56 +0530

    routh created (basic)

--- AdminServer.v
+++ AdminServer.v
@@ -11,8 +11,32 @@ struct App {
 pub fn (mut app App) index() vweb.Result {
     return $vweb.html()
 }
-@["/hello"]
-pub fn (mut app App) data() vweb.Result {
+@["/login"]
+pub fn (mut app App) login() vweb.Result {
+    return $vweb.html()
+}
+@["/dashboard"]
+pub fn (mut app App) dashboard() vweb.Result {
+    return $vweb.html()
+}
+@["/document"]
+pub fn (mut app App) document() vweb.Result {
+    return $vweb.html()
+}
+@["/event"]
+pub fn (mut app App) event() vweb.Result {
+    return $vweb.html()
+}
+@["/podcast"]
+pub fn (mut app App) podcast() vweb.Result {
+    return $vweb.html()
+}
+@["/member"]
+pub fn (mut app App) member() vweb.Result {
+    return $vweb.html()
+}
+@["/project"]
+pub fn (mut app App) project() vweb.Result {
     return $vweb.html()
 }
 
